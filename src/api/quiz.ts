db7efc4508aac8f40e17aea08d08c99c4169803b
FUKADA0118 <fukada0118.cs@gmail.com>
2022-06-24 12:09:17 +0100

    add quiz file

---
+++ src/api/quiz.ts
@@ -0,0 +1,173 @@
+const quiz = [
+    {
+        topic: "Cultura Geral",
+        questions:[
+            {
+
+                question:"Qual é o país com a maior linha de costa do mundo?",
+                options: ["Rússia","Canadá","Indonesia","EUA"],
+                answer: 2,
+            },
+            {
+
+                question:"Qual é o país com o maior número de população do mundo?",
+                options: ["Índia","EUA","Russia","China"],
+                answer: 4,
+            },
+            {
+
+                question:"Qual é a capital das Filipinas?",
+                options: ["Manila","Jakarta","Honolulu","Díli"],
+                answer: 1,
+            },
+            {
+
+                question:"A que país pertence a Grande Barreira de Coral?",
+                options: ["Nova Zelândia","Austrália","Indonesia","Japão"],
+                answer: 2,
+            },
+            {
+
+                question:"Qual é o país mais pequeno do mundo?",
+                options: ["Lichteinstein","Luxemburgo","Mónaco","Vaticano"],
+                answer: 4,
+            },
+            {
+
+                question:"Qual é o país com a cascata mais alta do mundo?",
+                options: ["EUA","Brasil","África do Sul","Venezuela"],
+                answer: 4,
+            },
+            {
+
+                question:"Qual é a capital da Austrália?",
+                options: ["Sydney","Adelaide","Canberra","Melbourne"],
+                answer: 3,
+            },
+            {
+
+                question:"Em que país se localiza o Monte Fuji?",
+                options: ["Perú","China","Japão","Coreia do Norte"],
+                answer: 3,
+            },
+            {
+
+                question:"Em que país se localiza o Cabo da Boa Esperança?",
+                options: ["EUA","África do Sul","Austrália","Canadá"],
+                answer: 2,
+            },
+            {
+
+                question:"Qual é o país com o maior número de vulcões ativos?",
+                options: ["Indonésia","Itália","Japão","Filipinas"],
+                answer: 1,
+            }
+
+
+        ]
+    },
+    {
+        topic: "História Portuguesa",
+        questions:[
+            {
+
+                question:"Que nome se dá à reconquista do território aos Muçulmanos?",
+                options: ["Reconquista Muçulmana","Reconquista Romana", "Reconsquista Cristã"],
+                answer: 3,
+            },
+            {
+
+                question:"Quem foram os dois cavaleiros que vieram ajudar os reis cristãos na luta contra os Muçulmanos?",
+                options: ["D. Afonso Henriques e D. Raimundo","D. Raimundo e D. Henrique","Afonso VI e D. Raimundo"],
+                answer: 2,
+            },
+            {
+
+                question:"Quem foi o pai de D. Teresa?",
+                options: ["D. Afonso VI de Leão","D. Afonso VII de Leão e Castela", "D. Raimundo"],
+                answer: 1,
+            },
+            {
+
+                question:"D. Teresa e D. Urraca eram...",
+                options: ["Primas","Cunhadas", "Irmãs"],
+                answer: 3,
+            },
+            {
+
+                question:"A que reino pertencia o Condado Portucalense?",
+                options: ["Reino de Castela","Reino de Leão","Reino de Aragão"],
+                answer: 2,
+            },
+            {
+
+                question:"Que obrigações tinha D. Henrique para com D. Afonso VI?",
+                options: ["Obediência e apoio militar","Obediência e apoio psicológico","Apoio militar e assistência médica"],
+                answer: 1,
+            },
+            {
+
+                question:"Quem eram os pais de D. Afonso Henriques?",
+                options: ["D. Urraca e D. Raimundo","D. Henrique e D. Urraca", "D. Henrique e D. Teresa"],
+                answer: 3,
+            },
+            {
+
+                question:"O filho de D. Raimundo e D. Urraca era...",
+                options: ["D. Afonso VII","D. Afonso Henriques","D. Afonso VI"],
+                answer: 1,
+            },
+            {
+
+                question:"D. Afonso Henriques e D. Afonso VII eram...",
+                options: ["Irmãos","Tio e Sobrinho", "Primos"],
+                answer: 3,
+            },
+            {
+
+                question:"D. Afonso Henriques derrotou o exército de D. Teresa em que batalha?",
+                options: ["Batalha de S. Mamede","Batalha de Ourique", "Batalha de Cerneja"],
+                answer: 1,
+            },
+
+        ]
+    },
+    {
+        topic: "Video-jogos",
+        questions:[
+            {
+
+                question:"",
+                options: ["",""],
+                answer: 0,
+            },
+            {
+
+                question:"",
+                options: ["",""],
+                answer: 0,
+            },
+            {
+
+                question:"",
+                options: ["",""],
+                answer: 0,
+            },
+            {
+
+                question:"",
+                options: ["",""],
+                answer: 0,
+            },
+            {
+
+                question:"",
+                options: ["",""],
+                answer: 0,
+            },
+            
+        ]
+    }
+]
+
+export default quiz;
